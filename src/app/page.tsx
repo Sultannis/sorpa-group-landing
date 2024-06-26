ba2a7928e03f38e2f8fa0beac0b816e7c20e4912
Sultannis <sultan.fb29@gmail.com>
2024-06-26 22:26:10 +0500

    feat: create sorpa landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,89 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <div className={styles.mainLogoWrapper}>
             <Image
-              src="/vercel.svg"
+              src="/sorpa-logo.svg"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              className={styles.mainLogo}
+              width={1}
+              height={1}
               priority
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </div>
+          <div className={styles.general}>
+            <h1>Sorpa group</h1>
+            <h3>Support email</h3>
+            <span>sultan.fb29@gmail.com</span>
+          </div>
+        </header>
+        <section className={styles.mobileGames}>
+          <h1>Mobile games</h1>
+          <ul className={styles.mobileGamesList}>
+            <li className={styles.mobileGameItem}>
+              <Image
+                src="/leep-up-logo.svg"
+                alt="Leep up logo"
+                className={styles.mobileGameLogo}
+                width={1}
+                height={1}
+                priority
+              />
+              <div className={styles.mobileGameInfo}>
+                <h2 className={styles.mobileGameName}>LeepUp</h2>
+                <span className={styles.mobileGameSubtitle}>
+                  Rise upwards with each step
+                </span>
+                <div className={styles.mobileGameLinks}>
+                  <a
+                    className={styles.mobileGameLinkButton}
+                    href="https://play.google.com/store/apps/details?id=com.sorpa.leepup"
+                  >
+                    <Image
+                      src="/google-icon-dark.svg"
+                      alt="Google Play"
+                      className={styles.mobileGameGoogleIconDark}
+                      width={1}
+                      height={1}
+                      priority
+                    />
+                    <Image
+                      src="/google-icon-light.svg"
+                      alt="Google Play"
+                      className={styles.mobileGameGoogleIconLight}
+                      width={1}
+                      height={1}
+                      priority
+                    />
+                  </a>
+                  <a
+                    className={styles.mobileGameLinkButton}
+                    href="https://apps.apple.com/us/app/leepup/id1580561961"
+                  >
+                    <Image
+                      src="/apple-icon-dark.svg"
+                      alt="App Store"
+                      className={styles.mobileGameAppleIconDark}
+                      width={1}
+                      height={1}
+                      priority
+                    />
+                    <Image
+                      src="/apple-icon-light.svg"
+                      alt="App Store"
+                      className={styles.mobileGameAppleIconLight}
+                      width={1}
+                      height={1}
+                      priority
+                    />
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </section>
       </div>
     </main>
   );
